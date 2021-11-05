fb935a72c5b25e44e2a679a926b88e70c2675247
Utkarsh Saraswat <Utkarsh>
2021-11-05 23:10:14 +0530

    fixed vis objects return value issue

--- systemdef.js
+++ systemdef.js
@@ -4,12 +4,13 @@ const simulation_file_name = require('./config.json').simulation;
 // const second_order_ode  = require('./simulations/second_order_ode.js')
 // const second_order_ode_advanced  = require('./simulations/second_order_ode_advanced.js')
 // const simple_chain  = require('./simulations/simple_chain.js')
-const simple_laplace  = require('./simulations/simple_laplace.js')
+// const simple_laplace  = require('./simulations/simple_laplace.js')
 // const cellmech = require('./simulations/cellmech.js')
 // const basic_curve = require(`./simulations/basic_curve.js`);
 // const plug_flow_reactor = require('./simulations/plug_flow_reactor.js')
 // const simulation_to_run = require(`./simulations/${simulation_file_name}`);
+const livebeehive = require('./simulations/livebeehive.js');
 
 reset();
-const system = simple_laplace;
+const system = livebeehive;
 module.exports.run = CONNECTIONS( system.main, system.Sparent);
