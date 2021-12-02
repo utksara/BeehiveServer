c5cebbc14b31bb0e91cd0d4ba34e130777e1d838
Utkarsh Saraswat <Utkarsh>
2021-12-02 19:35:57 +0530

    added main binary

--- simulations/livebeehive.js
+++ simulations/livebeehive.js
@@ -1,41 +1,60 @@
-          const {shapes, calc, SYSTEM, SIMPLECONNECT, CHAIN, STACK, MESH, CONNECTIONS, COPY, bfsTraverse }  = require('./../dev.js');
+const { PATTERN, DebugAny } = require('../lib/beehive.js');
+const {shapes, calc, SYSTEM, SIMPLECONNECT, BICONNECT, CONNECT, CHAIN, STACK, MESH, CONNECTIONS, COPY, bfsTraverse, traverse }  = require('../dev.js');
 
 shapes._reset();
 
+const Omega = 10; 
+const Dx = 0.01;
 
 let Sparent = SYSTEM();
 
+let calculate_traction = (async function (S){with (S){
+    T = T - omega*dx*T
+}})
+
+let increment_cellboundary = (async function (S){with (S){
+    
+    R = R + 10;
+    new_cell_boundary = [];
+
+    for (i = 0 ; i<cellboundary.length ; i++){
+        new_cell_boundary.push(cellboundary[i] * R * 0.01)
+    }
+
+    cellboundary = new_cell_boundary
+}})
+
 let S1 = SYSTEM ({
     NAME : "S1",
-    VISUALIZE : [
+    T : 200,
+    R : 50, 
+    dx : Dx,
+    omega : 10,
+    center: [600, 350],
+    cellboundary : [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100,], 
+    REQUIRE : ["T", "R",],    
+    VISUALIZE :[
         {
-            REPRESENTS : "Pressure",
-            GEOMETRY : shapes.line
+            REPRESENTS : "T",
+            GEOMETRY : shapes.curve,
+            POSITION : [650,350],
+            MOVEMENT : "cellboundary",
+            maxval : 200,
+            minval : -200,
         }
     ],
-    Pressure : 200,
-    REQUIRE : ["Pressure"],    
     PROCESSES : [
-        (async function (S){with (S){
-            Pressure = 0.95 * Pressure
-        }})
+        calculate_traction, 
+        increment_cellboundary,
     ],
 });
 
 let main = () => {
     //-----Example 1-----------
-        
-    let N = 50;
-    let PressureGen =(N)=> {
-        Pressurearray = []
-        for (let n = 0; n<N; n++){
-            Pressurearray.push(200 - 8*n);
-        }
-        return Pressurearray;
-    }
-    SIMPLECONNECT (Sparent) (CHAIN (S1, N));
+    let N = 10; 
+    SIMPLECONNECT (Sparent) (CHAIN(S1, N))
     // bfsTraverse(Sparent, arg =>{
-    //     console.log(arg.VISUALIZE[0])
+    //     console.log(arg.cellshape)
     // })
 }
 
